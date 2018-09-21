3bb332ce88b69238a8994a5610f6854669017a66
Andrey Gorokhov <36040108+andreyalexeich@users.noreply.github.com>
2018-09-21 15:06:44 +0300

    Fixes

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,7 +2,7 @@ module.exports = function() {
     $.gulp.task("scripts", function() {
         return $.gulp.src("./src/js/**/main.js")
             .pipe($.gp.sourcemaps.init())
-            .pipe($.gp.babel({ presets: ["@babel/preset-env"]}))
+            .pipe($.gp.babel({presets: ["@babel/preset-env"]}))
             .pipe($.gp.uglify())
             .pipe($.gp.rename({suffix: ".min"}))
             .pipe($.gp.sourcemaps.write("./maps/"))
